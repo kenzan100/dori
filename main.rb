2f129b6ced3fb5c9a6741d7640267401b6b7ce52
Yuta Miyama <s04155yo@gmail.com>
2021-02-22 20:37:07 -0500

    Demonstrate Write.new

--- main.rb
+++ main.rb
@@ -7,6 +7,7 @@ class Dori::Application
     # - Access
 
     # Adj list (forward)
+    # key =(data)=> [values]
     [
       reader1 => [writer1],
       reader2 => [writer1],
@@ -15,9 +16,19 @@ class Dori::Application
     ]
 
     # Adj list (inverse)
+    # key <=(data)= [values]
     [
       writer1 => [reader1, reader2, reader3, reader4]
     ]
+
+    # class DataComponent
+    #   def call(input)
+    #     output = transform(input)
+    #     return output
+    #   end
+    # end
+
+    ---
 
     # frequency: once
     reader1 = Read.new(
@@ -26,33 +37,78 @@ class Dori::Application
         format: :xml,
         must_contain: "//*[@class='mod_areaMap area']//div//ul//li//a"
       }
-    )
-    reader2 = Read.new(
-    )
+    ) # => dump the whatever data this was able to get as long its valid
+    # reader2 = Read.new()
 
+    # | Merge.new(
+    # )
+    # Select.new(
+    #   "//*[@class='mod_areaMap area']//div//ul//li//a"
+    # ).each do |element|
+    #   Read.new(
+    #     url: element,
+    #     validation: {
+    #       format: :xml,
+    #       must_contain: "//*[@class='mod_defTable rent_graphTable js_graph']//tbody//tr"
+    #     }
+    #   )
+    # end
 
-    | Merge.new(
-    )
-    Select.new(
-      "//*[@class='mod_areaMap area']//div//ul//li//a"
-    ).each do |element|
-      Read.new(
-        url: element,
-        validation: {
-          format: :xml,
-          must_contain: "//*[@class='mod_defTable rent_graphTable js_graph']//tbody//tr"
-        }
-      )
-    end
+    Write.new(json_schema, reader1, reader2)
 
-    schema = [
+    chintai_schema = [
       {
-        rent_average: matcher,
-        net_speed: matcher,
+        area: :name
       }
     ]
 
-    Write.call('hoge.json', schema)
+    Write.new(
+      chintai_schema,
+      reader1
+    )
+
+    dynamic_schema = [
+      {
+        rent_average: -> () { |elem, acc| acc + (elem/acc.len) },
+        net_speed:    -> () { |elem, acc| acc + (elem/acc.len) },
+      }
+    ]
+
+    proc = -> (str, acc) { acc + 1 }
+
+    enum = [1, 2, 3].each
+
+    class Write
+      def self.call(data_path, dynamic_schema, scan_rule)
+        data = File.read data_path
+        elements = data.scan(scan_rule).to_enum
+
+        memo = dynamic_schema.last
+        elements.size.times do
+          enum_val = elements.next
+
+          dynamic_schema.first.each do |k, procs|
+            proc = procs.first
+            default = procs.last
+            memo[k] = proc.call(enum_val, memo[k] || default)
+          end
+        end
+
+        memo
+      end
+    end
+
+    scan_rule = /style=(.+)>/
+    data_path = 'chintai_dump.xml'
+    dynamic_schema = [
+      {
+        cnt1: [-> (val, acc) { acc + 1 }, 0],
+        cnt2: [-> (val, acc) { acc + 1 }, 0],
+      },
+      {} # default
+    ]
+
+    Write.call(data_path, dynamic_schema, scan_rule)
 
     Read.new(
       key: [:id, :timestamp],
---
+++ write.rb
@@ -0,0 +1,34 @@
+require 'pp'
+require 'byebug'
+
+class Write
+  def self.call(data_path, dynamic_schema, scan_rule)
+    data = File.read data_path
+    elements = data.scan(scan_rule).flatten.each
+
+    memo = dynamic_schema.last
+    elements.size.times do
+      enum_val = elements.next
+
+      dynamic_schema.first.each do |k, procs|
+        proc = procs.first
+        default = procs.last
+        memo[k] = proc.call(enum_val, memo[k] || default)
+      end
+    end
+
+    memo
+  end
+end
+
+scan_rule = /style=(.+)>/
+data_path = 'chintai_dump.xml'
+dynamic_schema = [
+  {
+    cnt1: [-> (val, acc) { val.size + acc }, 0],
+    cnt2: [-> (val, acc) { acc + 1 }, 0],
+  },
+  {} # default
+]
+
+pp Write.call(data_path, dynamic_schema, scan_rule)
